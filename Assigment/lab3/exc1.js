fa61a2e820590919cc468b41543525b4ac224150
reddaGz <44gebre@gmail.com>
2021-06-26 00:47:53 -0500

    lab3 solution

---
+++ Assigment/lab3/exc1.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const http = require('http');
+    http.createServer((req, res) => {
+        if (req.url === '/') {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+          fs.createReadStream('index.html').pipe(res); 
+        }
+       else if (req.url === '/messsage' && req.method === 'POST') {
+            const body = [];
+            req.on('data', (chunk) => {
+                body.push(chunk);
+                fs.writeFileSync('text.txt', Buffer.concat(body).
+                toString().split('=')[1]);
+            });
+            res.statusCode = 302;
+            res.setHeader("Location", "/");
+            return res.end();
+        }   
+    }).listen(443);
